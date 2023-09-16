b88605fb9033c0bf8d72f38f314f026e1a39a2c5
Robert Kovats <robertrobertzicku@yahoo.com>
2023-09-16 20:05:12 +0300

    CSSs commented. Useless code removed.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,35 +8,10 @@ defineProps({
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      I’m Wolf Onix.
-    </h3>
-  </div>
+  <h2>Hello there!</h2>
+  <h1>{{msg}}</h1>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
